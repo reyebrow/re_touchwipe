978ce577e04963cfd1ce7fe57bf7c00f337efb42
Colin Calnan <colin@colincalnan.com>
2011-10-19 15:01:17 -0700

    First commit" "

---
+++ README.txt
@@ -0,0 +1,1 @@
+Use this module in conjunction with the RE Context Libraries module to allow slideshows to be swipeable on touch devices.
---
+++ js/re_jquerytouchwipe.js
@@ -0,0 +1,26 @@
+/**
+ * @file
+ * This file provides whatever javascript logic and functions are necessary for
+ * the jquery cycle plugin in the re_contextlibraries module.
+ *
+ * Originally developed for use with the re_slideshow feature. You can override
+ * this file in the context where the library is invoked.
+ *
+ * @see http://www.netcu.de/jquery-touchwipe-iphone-ipad-library
+ */
+(function ($) {
+  Drupal.behaviors.re_jquerytouchwipe = {
+    attach: function(context) {
+    var $slideshow_container =  $('#block-views-re-slideshow-block .view-display-id-block > .view-content > ul');
+    $slideshow_container
+      .touchwipe({
+        wipeLeft: function() {
+          $slideshow_container.cycle("next");
+        },
+        wipeRight: function() {
+          $slideshow_container.cycle("prev");
+        }
+      });
+    }
+  }; /* Drupal.behaviors.re_jquerytouchwipe */
+})(jQuery);
